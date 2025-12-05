303b9f59d7c8ca9fcf7cd770c66fa48005dc3f20
RENATO MESQUITA DE SOUZA <renato.mesquita@aluno.senac.rio>
2025-12-05 19:17:23 -0300

    meu primeiro commit

---
+++ app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import *  # 1. Importe o Model que você criou
+
+# 2. Registre o seu Model no site de admin
+admin.site.register(Curso)
+admin.site.register(Plano)
